4c6617d57173d3f97501dcacdf37fc3d08cb8980
Saniya <saniyabinoy22@gmail.com>
2022-11-17 18:11:45 +0530

    basic xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,72 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: MaterialButton(
-            onPressed: () {
-              final player = AudioPlayer();
-              player.play(AssetSource('note1.wav'),
-              );
-            },
-            enableFeedback: false,
+          child: Column(
+            children: [
+              MaterialButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note1.wav'),
+                  );
+                },
+                enableFeedback: false,
+                color: Colors.red,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note2.wav'),
+                  );
+                },
+                enableFeedback: false,
+                color: Colors.orange,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note3.wav'),
+                  );
+                },
+                enableFeedback: false,
+                color: Colors.yellow,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note4.wav'),
+                  );
+                },
+                enableFeedback: false,
+                color: Colors.green,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note5.wav'),
+                  );
+                },
+                enableFeedback: false,
+                color: Colors.blue,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note6.wav'),
+                  );
+                },
+                enableFeedback: false,
+                color: Colors.indigo,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note2.wav'),
+                  );
+                },
+                enableFeedback: false,
+                color: Colors.purple.shade900,
+              ),
+            ],
           ),
         ),
       ),
